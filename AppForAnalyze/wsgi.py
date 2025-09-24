e5aada1d5d5b17c483ed8cc6c0cb5018c831fa02
Pockord <oleg222200005555@gmail.com>
2025-09-24 22:06:34 +0300

    First init of app

---
+++ AppForAnalyze/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AppForAnalyze project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppForAnalyze.settings')
+
+application = get_wsgi_application()
