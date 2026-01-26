d318d960eea7b2855f5383a8274e0e82b4b8f6e9
Pockord <oleg222200005555@gmail.com>
2026-01-26 04:53:32 +0300

    bug with scope param was fixed

--- catalog/templatetags/articles_tags.py
+++ catalog/templatetags/articles_tags.py
@@ -4,11 +4,19 @@ from django.core.paginator import Paginator
 
 register = template.Library()
 
+SERVICE_PARAMS = {'scope'}
+
 @register.simple_tag(takes_context=True)
 def change_params(context, **kwargs):
-    query = context['request'].GET.dict()
-    query.update(kwargs)
-    return urlencode(query)
+    query = context['request'].GET.copy()
+
+    for i in SERVICE_PARAMS:
+        query.pop(i, None)
+
+    for key, value in kwargs.items():
+        query[key] = value
+    
+    return query.urlencode()
 
 @register.simple_tag
 def get_proper_elided_page_range(p, number, on_each_side=2, on_ends=1):
