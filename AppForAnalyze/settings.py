9b87f7786b2250a8548a60e35277c1b80ef95d42
Pockord <oleg222200005555@gmail.com>
2026-02-03 18:37:18 +0300

    fixed static collect problem

--- AppForAnalyze/settings.py
+++ AppForAnalyze/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
