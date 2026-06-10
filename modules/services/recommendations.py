3888917899541f540bd25960295938df599d9619
Pockord <oleg222200005555@gmail.com>
2026-06-10 13:48:24 +0300

    Swap recomendations waithout abstract from trigram to cosinedist

--- modules/services/recommendations.py
+++ modules/services/recommendations.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 from pgvector.django import CosineDistance
-from django.contrib.postgres.search import TrigramSimilarity
+from common.ml.sentence_transformer_model import get_model
 
 from main.models import Artical, ArticalEmbedding
 
@@ -22,23 +22,31 @@ def get_article_recommendations(article_id):
                 .annotate(distance=CosineDistance("embedding", abstract.embedding))
                 .order_by("distance")[:3])
             
-            # ids = qs.values_list('article_id', flat=True)
             ids = [i.article_id for i in qs]
 
             qs = Artical.objects.filter(id__in = ids)
 
         else:
+            model = get_model()
+            title_embedding = model.encode(art.title, device='cpu', normalize_embeddings=True).tolist()
+
             qs = (
-                Artical.objects.exclude(id = article_id)
-                .annotate(similarity=TrigramSimilarity("title", art.title))
-                .order_by("-similarity")[:3])
+                ArticalEmbedding.objects.annotate(distance=CosineDistance("embedding", title_embedding))
+                .order_by("distance")[:3])
+            
+            # qs = (
+            #     Artical.objects.exclude(id = article_id)
+            #     .annotate(similarity=TrigramSimilarity("title", art.title))
+            #     .order_by("-similarity")[:3])
             
             ids = [i.id for i in qs]
+
+            qs = Artical.objects.filter(id__in = ids)
+                
 
     except Artical.DoesNotExist:
         return []
     
-
     cache.set(cache_key, ids, 60 * 30)
 
     return qs    
