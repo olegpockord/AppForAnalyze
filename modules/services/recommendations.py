bd84b4b794c1533d2a71012b800c1d86735b5807
Pockord <oleg222200005555@gmail.com>
2026-06-15 22:38:24 +0300

    serialized recommendations qs in list which fixed double query, added caching to recommendations

--- modules/services/recommendations.py
+++ modules/services/recommendations.py
@@ -7,19 +7,18 @@ from django.db.models import F, IntegerField, Value
 from main.models import Artical, ArticalEmbedding
 
 def get_article_recommendations(article_id):
-    # cache_key = f"recommendation_№{article_id}"
+    cache_key = f"recommendation_№{article_id}"
 
-    # cached = cache.get(cache_key)
+    cached = cache.get(cache_key)
 
-    # if cached:
-    #     qs = Artical.objects.filter(id__in = cached)
-    #     return qs
+    if cached:
+        return cached
     try:
         art = Artical.objects.get(id = article_id)
         abstract = art.abstract.first()
 
         if abstract and abstract.embedding is not None:
-            qs = (
+            qs = list(
                 ArticalEmbedding.objects.exclude(article__id = article_id)
                 .annotate(distance=CosineDistance("embedding", abstract.embedding))
                 .annotate(similarity=ExpressionWrapper((Value(1) - F("distance")) * 100, output_field=IntegerField()))
@@ -31,13 +30,15 @@ def get_article_recommendations(article_id):
                     article_title=F("article__title"),
                     article_pk=F("article__id")
                 )
-                .order_by("distance")[:3])
+                .order_by("distance")[:3]
+                .values("similarity", "date", "citations_in_article", "main_author", "journal_name", "article_title", "article_pk")
+                )
 
         else:
             model = get_model()
             title_embedding = model.encode(art.title, device='cpu', normalize_embeddings=True).tolist()
 
-            qs = (
+            qs = list(
                 ArticalEmbedding.objects.exclude(article__id = article_id)
                 .annotate(distance=CosineDistance("embedding", title_embedding))
                 .annotate(similarity=ExpressionWrapper((Value(1) - F("distance")) * 100, output_field=IntegerField()))
@@ -45,14 +46,17 @@ def get_article_recommendations(article_id):
                     date=F("article__articaldate__date_of_artical"),
                     citations_in_article=F("article__articalcitedata__reference_count"),
                     main_author=F("article__articlemainauthor__main_initials"),
+                    journal_name=F("article__articalciteinformation__journal_name"),
                     article_title=F("article__title"),
                     article_pk=F("article__id")
                 )
-                .order_by("distance")[:3])
+                .order_by("distance")[:3]
+                .values("similarity", "date", "citations_in_article", "main_author", "journal_name", "article_title", "article_pk")
+                )
                 
     except Artical.DoesNotExist:
         return []
     
-    # cache.set(cache_key, ids, 60 * 30)
+    cache.set(cache_key, qs, 60 * 30)
 
     return qs    
