f287b1c7b886e205f89b894b57f531520033d74f
Pockord <oleg222200005555@gmail.com>
2026-09-04 14:55:54 +0300

    Fixed setting for launch via docker

--- AppForAnalyze/settings.py
+++ AppForAnalyze/settings.py
@@ -138,7 +138,7 @@ DATABASES = {
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',           #env('POSTGRES_HOST'),
+        'HOST': env('POSTGRES_HOST'),
         'PORT': env('POSTGRES_PORT'),
     }
 }
